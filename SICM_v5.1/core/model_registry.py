d69658cf6aea305c874c361d03cd01fd5f2633d5
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-06-23 16:58:09 -0500

    Add model registry with available models

---
+++ SICM_v5.1/core/model_registry.py
@@ -0,0 +1,18 @@
+AVAILABLE_MODELS = {
+
+    "islm":
+
+        "Keynesiano Cerrado",
+
+    "mundell_fleming":
+
+        "Keynesiano Abierto",
+
+    "classical_closed":
+
+        "Clásico Cerrado",
+
+    "classical_open":
+
+        "Clásico Abierto"
+}
