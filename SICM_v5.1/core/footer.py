104dd9236bb677c83a30e15108638825c6a392ef
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-06-24 09:37:53 -0500

    Add footer rendering function in footer.py
    Footer institucional

---
+++ SICM_v5.1/core/footer.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+
+def render_footer():
+
+    st.markdown("---")
+
+    st.caption(
+        """
+        SICM v5.1 Research Lab
+
+        Desarrollado por Edinson Patrocinio Valencia Omaña
+        UNIVERSIDAD NACIONAL DE COLOMBIA
+        
+        edvalenciao@unal.edu.co
+        """
+    )
