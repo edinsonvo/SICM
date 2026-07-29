6d59dec668a26e253d75fb259000a22e90197404
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-07-28 22:38:10 -0500

    Implement Registry class for model management
    Added a Registry class for model registration and creation.

--- SICM_v5.1_RC1/core/registry.py
+++ SICM_v5.1_RC1/core/registry.py
@@ -1,1 +1,36 @@
+class Registry:
 
+    _models = {}
+
+    @classmethod
+    def register(
+
+        cls,
+
+        name,
+
+        model
+
+    ):
+
+        cls._models[name] = model
+
+    def create(
+
+        self,
+
+        name,
+
+        config
+
+    ):
+
+        if name not in self._models:
+
+            raise ValueError(
+
+                f"Modelo {name} no registrado."
+
+            )
+
+        return self._models[name](config)
