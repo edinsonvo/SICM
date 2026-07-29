01bd4ef534138b01a224052098494b4ff7734d9a
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-07-28 22:37:33 -0500

    Add Engine class with model registry functionality

--- SICM_v5.1_RC1/core/engine.py
+++ SICM_v5.1_RC1/core/engine.py
@@ -1,1 +1,28 @@
+from core.registry import Registry
 
+
+class Engine:
+
+    def __init__(self):
+
+        self.registry = Registry()
+
+    def run(
+
+        self,
+
+        model_name,
+
+        config
+
+    ):
+
+        model = self.registry.create(
+
+            model_name,
+
+            config
+
+        )
+
+        return model.solve()
