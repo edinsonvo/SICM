722904bd3ce750f82fb7a014d1ee975a92b67bc7
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-07-28 22:38:56 -0500

    Define BaseModel with abstract methods
    Added abstract methods for solving, interpreting, and metrics.

--- SICM_v5.1_RC1/models/base_model.py
+++ SICM_v5.1_RC1/models/base_model.py
@@ -1,1 +1,31 @@
+from abc import ABC
 
+from abc import abstractmethod
+
+
+class BaseModel(ABC):
+
+    def __init__(
+
+        self,
+
+        config
+
+    ):
+
+        self.config = config
+
+    @abstractmethod
+    def solve(self):
+
+        ...
+
+    @abstractmethod
+    def interpret(self):
+
+        ...
+
+    @abstractmethod
+    def metrics(self):
+
+        ...
