651cf34cf80fffde159c98cc2d3c38eff8823dee
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-07-28 22:57:16 -0500

    Implement MundellFlemingModel class
    Added the MundellFlemingModel class with methods for solving, interpreting, and plotting.

--- SICM_v5.1_RC1/models/keynesian/mundell_fleming.py
+++ SICM_v5.1_RC1/models/keynesian/mundell_fleming.py
@@ -1,1 +1,16 @@
+@Registry.register("mundell_fleming")
+class MundellFlemingModel(BaseModel):
 
+    ...
+
+    def solve():
+
+        ...
+
+    def interpret():
+
+        ...
+
+    def default_plot():
+
+        return "mf"
