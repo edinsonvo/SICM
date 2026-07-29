f76efcec52f0f6687e93159864a79d5697f67e86
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-07-28 22:39:32 -0500

    Add Streamlit integration for app interface

--- SICM_v5.1_RC1/app.py
+++ SICM_v5.1_RC1/app.py
@@ -1,1 +1,38 @@
+import streamlit as st
 
+from core.engine import Engine
+
+from ui.sidebar import sidebar
+
+from ui.dashboard import dashboard
+
+from ui.footer import footer
+
+
+st.set_page_config(
+
+    layout="wide"
+
+)
+
+engine = Engine()
+
+model, config = sidebar()
+
+if st.button(
+
+    "Simular"
+
+):
+
+    result = engine.run(
+
+        model,
+
+        config
+
+    )
+
+    dashboard(result)
+
+footer()
