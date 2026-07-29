cfdbb6008260ec38688902276db1f8143e5a4462
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-07-28 20:58:40 -0500

    Add helpers.py with equilibrium_marker and linspace functions

---
+++ SICM_v5.1/visualization/helpers.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def equilibrium_marker(x, y):
+
+    return dict(
+
+        x=[x],
+
+        y=[y]
+    )
+
+
+def linspace():
+
+    return np.linspace(
+
+        0,
+
+        1000,
+
+        200
+    )
