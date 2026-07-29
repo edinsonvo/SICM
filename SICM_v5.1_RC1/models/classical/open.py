763996b5defc1d74e7c9a10f20e8c2fa718fc912
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-07-28 22:58:09 -0500

    Add ClassicalOpenModel class with methods

--- SICM_v5.1_RC1/models/classical/open.py
+++ SICM_v5.1_RC1/models/classical/open.py
@@ -1,1 +1,16 @@
+@Registry.register("classical_open")
+class ClassicalOpenModel(BaseModel):
 
+    ...
+
+    def solve():
+
+        ...
+
+    def interpret():
+
+        ...
+
+    def default_plot():
+
+        return "production"
