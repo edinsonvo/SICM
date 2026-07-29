8202c2fc0f42c52380b1211092e115a110e9b542
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-07-28 22:57:45 -0500

    Add ClassicalClosedModel class with methods

--- SICM_v5.1_RC1/models/classical/closed.py
+++ SICM_v5.1_RC1/models/classical/closed.py
@@ -1,1 +1,16 @@
+@Registry.register("classical_closed")
+class ClassicalClosedModel(BaseModel):
 
+    ...
+
+    def solve():
+
+        ...
+
+    def interpret():
+
+        ...
+
+    def default_plot():
+
+        return "labor"
