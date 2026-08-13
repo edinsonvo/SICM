a0229d72715980c8977841d4334d86efaf87dbf8
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-08-13 12:28:10 -0500

    Add return type annotation to Dispatcher.dispatch method

---
+++ src/sicm_core/core/dispatcher.py
@@ -0,0 +1,17 @@
+from typing import Any, Type
+
+from sicm_core.core.registry import Registry
+
+
+class Dispatcher:
+
+    def __init__(
+        self,
+        registry: Registry
+    ) -> None:
+
+        self.registry = registry
+
+    def dispatch(self, model_name: str) -> Type[Any]:
+
+        return self.registry.get(model_name)
