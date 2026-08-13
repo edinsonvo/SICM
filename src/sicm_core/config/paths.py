7a72b7a4fab538f44d704f0c67698f026dd1eefa
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-08-13 11:12:15 -0500

    Add function to ensure log directory exists

---
+++ src/sicm_core/config/paths.py
@@ -0,0 +1,7 @@
+"""Project paths."""
+from pathlib import Path
+
+def ensure_log_directory(log_file: str) -> Path:
+    path = Path(log_file)
+    path.parent.mkdir(parents=True, exist_ok=True)
+    return path
