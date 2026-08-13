d57570f54719a773c3e81c66b82ab563697fb32f
Edinson Valencia <131030759+edinsonvo@users.noreply.github.com>
2026-08-13 12:28:23 -0500

    Add type annotations to Registry class methods and fix type hints

---
+++ src/sicm_core/core/registry.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+from typing import Type, Any
+
+from sicm_core.exceptions import ModelNotFoundError
+
+
+class Registry:
+
+    def __init__(self) -> None:
+
+        self._models: dict[str, Type[Any]] = {}
+
+    def register(self, model: Type[Any]) -> None:
+
+        self._models[model.name] = model
+
+    def get(self, name: str) -> Type[Any]:
+
+        try:
+
+            return self._models[name]
+
+        except KeyError:
+
+            raise ModelNotFoundError(name)
